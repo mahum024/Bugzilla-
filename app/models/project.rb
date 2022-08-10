541a3be740edd2e81b89d1ff8b571976c5abeec2
mahum024 <mahum.maqbool@devsinc.com>
2022-08-11 01:40:36 +0500

    Added validations in Project  model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  has_and_belongs_to_many :users
-  has_many :bugs
+  has_and_belongs_to_many :users, dependent: :destroy
+  has_many :bugs, dependent: :destroy
+  validates :name, presence: true
+
+ 
+
 end
