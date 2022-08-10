26a812a2d540dec70aa3cb481c8d7d2e6e94e638
mahum024 <mahum.maqbool@devsinc.com>
2022-08-10 21:18:24 +0500

    manager dashboard created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :projects
+  resources :projects do
+ 
+    get 'add_developer_qa'
+    match 'add/:id' => 'projects#add', :as => 'add_user', via: %i[get patch]
+    match 'remove/:id' => 'projects#remove', :as => 'remove_user', via: %i[get patch]
+  end
   resources :bugs
 end
