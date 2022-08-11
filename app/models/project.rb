69718b373e1c46d4ebd8c11732c24050b7b4e449
mahum024 <mahum.maqbool@devsinc.com>
2022-08-11 12:28:51 +0500

    Added some more model level validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,6 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :users, dependent: :destroy
   has_many :bugs, dependent: :destroy
-  validates :name, presence: true
 
- 
-
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
 end
