8f441ee4011d98d524e3a0e39d0f072d157772cf
mahum024 <mahum.maqbool@devsinc.com>
2022-08-12 20:30:27 +0500

    created call backs to make controller slim

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,8 @@
 
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
+  before_action :set_project_change, only: %i[add remove add_developer_qa]
+  before_action :set_user, only: %i[add remove]
 
   def index
     case current_user.user_type
@@ -47,29 +49,31 @@ class ProjectsController < ApplicationController
   end
 
   def add_developer_qa
-    @project = Project.find(params[:project_id])
     @developer_in_project = @project.users.reload.where(user_type: :developer)
     @developer_not_in_project = User.where(user_type: :developer) - @developer_in_project
     @qa_in_project = @project.users.reload.where(user_type: :qa)
     @qa_not_in_project = User.where(user_type: :qa) - @qa_in_project
   end
 
-  
   def add
-    @project = Project.find(params[:project_id])
-    @user = User.find(params[:id])
     @project.users << @user
     redirect_to project_add_developer_qa_path(@project)
   end
 
   def remove
-    @project = Project.find(params[:project_id])
-    @user = User.find(params[:id])
     @project.users.delete(@user)
     redirect_to project_add_developer_qa_path(@project)
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_project_change
+    @project = Project.find(params[:project_id])
+  end
 
   def set_project
     @project = Project.find(params[:id])
