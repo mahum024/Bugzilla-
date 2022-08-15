74264fe5292365fd75a655c83c84d80a80f028f1
mahum024 <mahum.maqbool@devsinc.com>
2022-08-15 16:01:52 +0500

    Seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+begin
+  user = User.new
+  user.name = 'Developer'
+  user.email = 'developer@devsinc.com'
+  user.password = '123456'
+  user.password_confirmation = '123456'
+  user.user_type = :developer
+  user.save!
+rescue ActiveRecord::RecordInvalid => invalid
+  puts invalid.record.errors.full_messages
+end
+
+begin
+ user = User.new
+ user.name = 'Admin'
+ user.email = 'admin@devsinc.com'
+ user.password = '123456'
+ user.password_confirmation = '123456'
+ user.user_type = :manager
+ user.save!
+rescue ActiveRecord::RecordInvalid => invalid
+  puts invalid.record.errors.full_messages
+end
+  
+begin
+   project1 = Project.create(name: "Project1",created_at: Date.today, updated_at: Date.today)
+rescue ActiveRecord::RecordInvalid => invalid
+    puts invalid.record.errors.full_messages
+end
+
+begin
+  bug1 = Bug.create(title: "Bug1", description: "New Bug", deadline: Date.today, type_of_bug: "feature", status: "new_bug", creator_id: User.first.id, developer_id: User
+  .first.id, created_at: Date.today, updated_at: Date.today, project_id: Project.first.id)
+rescue ActiveRecord::RecordInvalid => invalid
+    puts invalid.record.errors.full_messages
+end
+ 
